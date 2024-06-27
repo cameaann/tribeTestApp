df9691269d51228d5567c149fda05f598e3e467a
Anna <cameaann@gmail.com>
2024-06-27 11:35:35 +0300

    adding function to get array of weeks

--- src/service/getWeeksArray.js
+++ src/service/getWeeksArray.js
@@ -2,33 +2,39 @@ import dayjs from "dayjs";
 import weekOfYear from "dayjs/plugin/weekOfYear";
 dayjs.extend(weekOfYear);
 
-function getWeeksArray() {
-  const weeksArray = [];
-  const currentWeek = dayjs().week();
+const weeksArrays = {};
 
-  for (let i = 0; i < 7; i++) {
-    const weekStart = dayjs()
-      .week(currentWeek + i)
-      .startOf("week");
+function getWeeksArray(personId) {
 
-    const days = [];
+  if (!weeksArrays[personId]) {
+    const weeksArray = [];
+    const currentWeek = dayjs().week();
 
-    for (let j = 0; j < 7; j++) {
-      const day = weekStart.add(j, "day");
-      days.push({
-        id:dayjs(day).format("dd")+day.format("DD/MM"),
-        name: dayjs(day).format("dd"),
-        date: day.format("DD/MM"),
-        selected: false,
+    for (let i = 0; i < 7; i++) {
+      const weekStart = dayjs()
+        .week(currentWeek + i)
+        .startOf("week");
+
+      const days = [];
+
+      for (let j = 0; j < 7; j++) {
+        const day = weekStart.add(j, "day");
+        days.push({
+          id: dayjs(day).format("dd") + day.format("DD/MM"),
+          name: dayjs(day).format("dd"),
+          date: day.format("DD/MM"),
+          selected: false,
+        });
+      }
+
+      weeksArray.push({
+        week: currentWeek + i,
+        days: days,
       });
     }
-
-    weeksArray.push({
-      week: currentWeek + i,
-      days: days,
-    });
+    weeksArrays[personId] = weeksArray;
   }
-  return weeksArray;
+  return weeksArrays[personId];
 }
 
 export { getWeeksArray };
