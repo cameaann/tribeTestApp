d6b7140571edd7e78f593993d831c94b2921d912
Anna <cameaann@gmail.com>
2024-06-28 15:17:19 +0300

    adding FriendsInfo to Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,23 +1,21 @@
 import { useContext } from "react";
 import { UserContext } from "./UserContext";
 import Calendar from "./Calendar";
+import FriendsInfo from "./FriendsInfo";
 
 const Profile = () => {
   const { users, selectedUserId, updateUser } = useContext(UserContext);
   let currentUser = users[selectedUserId];
 
   const saveProfile = (weeksArray) => {
-    console.log(weeksArray);
     const userInfo = {...currentUser, calendar: weeksArray};
     updateUser(userInfo);
   }
 
   return (
-    <div className="availability-section">
-      <h3>{currentUser.name}&apos;s availability for the next 7 weeks</h3>
-
+    <div className="profile">
       <Calendar user={currentUser} userId={selectedUserId} saveCalendar={saveProfile}/>
-
+      <FriendsInfo currentUser={currentUser} users={users}/>
     </div>
   );
 };
