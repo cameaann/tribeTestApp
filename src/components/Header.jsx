e885fdc65be7fd7d47472cbcbecd92401ecbad7f
Anna <cameaann@gmail.com>
2024-06-28 15:11:11 +0300

    adding formatName

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,28 +2,33 @@ import { useContext } from "react";
 import { UserContext } from "./UserContext";
 
 const Header = () => {
+  const { users, selectedUserId, handleChange } = useContext(UserContext);
+  const handleOnChange = (event) => {
+    const selected = event.target.value;
+    handleChange(selected);
+  };
 
-    const { users, selectedUserId, handleChange } =  useContext(UserContext);
-    const handleOnChange = (event) =>{
-        const selected = event.target.value;
-        handleChange(selected)
-    }
+  const formatName = (val) =>{
+    let fName = val.slice(0, 2).toUpperCase();
+    return fName;
+  }
 
-    console.log(selectedUserId);
+  let av = formatName(users[selectedUserId].name);
 
   return (
     <div className="header">
+      <div>
+        <label>Select user</label>
+        <select className="select-user" onChange={handleOnChange}>
+          {users.map((user, index) => (
+            <option key={index} value={index}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
 
-      <span>Select user</span>
-      <select
-        className="select-user"
-        onChange={handleOnChange}
-      >
-        {users.map((user, index) => (
-          <option key={index} value={index}>{user.name}</option>
-        ))}
-      </select>
-      <span>{users[selectedUserId].name}&apos;s profile</span>
+      <span className="profile-sign">{av}</span>
     </div>
   );
 };
--- src/components/Week.jsx
+++ src/components/Week.jsx
@@ -1,4 +1,3 @@
-// import { useState } from "react";
 import Day from "./Day";
 
 const Week = ({week, days, toggleSelected}) => {
